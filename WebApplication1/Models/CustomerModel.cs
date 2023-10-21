697ccc987a6e24f8a81dbccd4a3561b5c1b876a8
xfarkas2 <xfarkas2@mail.fi.muni.cz>
2023-10-21 14:08:37 +0200

    Customer Model

---
+++ WebApplication1/Models/CustomerModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication1.Models
+{
+    public class CustomerModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
