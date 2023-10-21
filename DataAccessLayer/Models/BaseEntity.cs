d7fa709b68922d2a50cf274b304f23ee6695d872
Jakub Zovak <482857@mail.muni.cz>
2023-10-21 12:03:58 +0200

    Add Dal to project together with entities

---
+++ DataAccessLayer/Models/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAccessLayer.Models
+{
+    public class BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
---
+++ DataAccessLayer/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace DataAccessLayer.Models
+{
+    public class Book : BaseEntity
+    {
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+        public virtual Author Author { get; set; }
+        public int GenreId { get; set; }
+        public int PublisherId { get; set; }
+        public virtual Publisher Publisher { get; set; }
+        public decimal Price { get; set; }
+    }
+}
