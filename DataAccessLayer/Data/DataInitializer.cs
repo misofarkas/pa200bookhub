27a7547f488b0be2bf8533b4a5e89fe7c5667e23
Jakub Zovak <482857@mail.muni.cz>
2023-10-29 18:28:23 +0100

    Add additional seed data

--- DataAccessLayer/Data/DataInitializer.cs
+++ DataAccessLayer/Data/DataInitializer.cs
@@ -32,7 +32,9 @@ namespace DataAccessLayer.Data
             {
                 new Author { Id = 1, Name = "George Orwell" },
                 new Author { Id = 2, Name = "J.K. Rowling" },
-                new Author { Id = 3, Name = "J.R.R. Tolkien" }
+                new Author { Id = 3, Name = "J.R.R. Tolkien" },
+                new Author { Id = 4, Name = "Agatha Christie" },
+                new Author { Id = 5, Name = "Stephen King" }
             };
         }
 
@@ -42,7 +44,10 @@ namespace DataAccessLayer.Data
             {
                 new Genre { Id = 1, Name = "Fiction" },
                 new Genre { Id = 2, Name = "Fantasy" },
-                new Genre { Id = 3, Name = "Adventure" }
+                new Genre { Id = 3, Name = "Adventure" },
+                new Genre { Id = 4, Name = "Mystery" },
+                new Genre { Id = 5, Name = "Horror" }
+
             };
         }
 
@@ -53,7 +58,9 @@ namespace DataAccessLayer.Data
             {
                 new Publisher { Id = 1, Name = "Penguin Books" },
                 new Publisher { Id = 2, Name = "Bloomsbury" },
-                new Publisher { Id = 3, Name = "HarperCollins" }
+                new Publisher { Id = 3, Name = "HarperCollins" },
+                new Publisher { Id = 4, Name = "Simon & Schuster" },
+                new Publisher { Id = 5, Name = "Scholastic" }
             };
         }
 
@@ -63,7 +70,10 @@ namespace DataAccessLayer.Data
             {
                 new Book { Id = 1, Title = "1984", AuthorId = 1, GenreId = 1, PublisherId = 1, Price = 15.99M, Description = "Nineteen Eighty-Four (also published as 1984) is a dystopian novel and cautionary tale by English writer George Orwell. It was published on 8 June 1949" },
                 new Book { Id = 2, Title = "Harry Potter and the Philosopher's Stone", AuthorId = 2, GenreId = 2, PublisherId = 2, Price = 20.99M, Description = "Harry Potter and the Philosopher's Stone is a fantasy novel written by British author J. K. Rowling." },
-                new Book { Id = 3, Title = "The Hobbit", AuthorId = 3, GenreId = 3, PublisherId = 3, Price = 25.99M, Description = "The Hobbit, or There and Back Again is a children's fantasy novel by English author J. R. R. Tolkien." }
+                new Book { Id = 3, Title = "The Hobbit", AuthorId = 3, GenreId = 3, PublisherId = 3, Price = 25.99M, Description = "The Hobbit, or There and Back Again is a children's fantasy novel by English author J. R. R. Tolkien." },
+                new Book { Id = 4, Title = "Murder on the Orient Express", AuthorId = 4, GenreId = 4, PublisherId = 4, Price = 18.99M, Description = "Murder on the Orient Express is a detective novel by English writer Agatha Christie." },
+                new Book { Id = 5, Title = "The Shining", AuthorId = 5, GenreId = 5, PublisherId = 5, Price = 22.99M, Description = "The Shining is a horror novel by American author Stephen King." }
+
             };
         }
 
@@ -71,7 +81,9 @@ namespace DataAccessLayer.Data
         {
             return new List<Customer>()
             {
-                new Customer { Id = 1, Username = "Janko", Password = "password123" }
+                new Customer { Id = 1, Username = "Janko", Password = "password123" },
+                new Customer { Id = 2, Username = "AnnaB", Password = "annaBsecure456" },
+                new Customer { Id = 3, Username = "MikeW", Password = "mikeWpassword789" }
             };
         }
 
@@ -80,7 +92,10 @@ namespace DataAccessLayer.Data
             return new List<PurchaseHistory> 
             { 
                 new PurchaseHistory { Id = 1, BookId = 1, CustomerId = 1, PurchaseDate = new DateTime(2023, 10, 10) },
-                new PurchaseHistory { Id = 2, BookId = 3, CustomerId = 1, PurchaseDate = new DateTime(2023, 8, 15) }
+                new PurchaseHistory { Id = 2, BookId = 3, CustomerId = 1, PurchaseDate = new DateTime(2023, 8, 15) },
+                new PurchaseHistory { Id = 3, BookId = 4, CustomerId = 2, PurchaseDate = new DateTime(2023, 9, 5) },
+                new PurchaseHistory { Id = 4, BookId = 5, CustomerId = 3, PurchaseDate = new DateTime(2023, 7, 20) }
+
             };
         }
 
@@ -90,6 +105,8 @@ namespace DataAccessLayer.Data
             {
                 new Review { Id = 1, BookId = 1, CustomerId = 1, Rating = 5, Comment = "Very nice book!"},
                 new Review { Id = 2, BookId = 3, CustomerId = 1, Rating = 1, Comment = "Worst book!"},
+                new Review { Id = 3, BookId = 4, CustomerId = 2, Rating = 4, Comment = "Intriguing and thrilling!" },
+                new Review { Id = 4, BookId = 5, CustomerId = 3, Rating = 3, Comment = "Scary but a bit too long." }
             };
         
         }
@@ -98,7 +115,9 @@ namespace DataAccessLayer.Data
         {
             return new List<Wishlist>()
             {
-                new Wishlist { Id = 1, BookId = 2, CustomerId = 1}
+                new Wishlist { Id = 1, BookId = 2, CustomerId = 1},
+                new Wishlist { Id = 2, BookId = 4, CustomerId = 2 },
+                new Wishlist { Id = 3, BookId = 5, CustomerId = 3 }
             };
         }
 
