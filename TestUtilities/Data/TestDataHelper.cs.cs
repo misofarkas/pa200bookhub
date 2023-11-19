0a96758b17286f1a10d78b516ea1041223401183
Peter <xgregus2@fi.muni.cz>
2023-11-19 15:47:23 +0100

    fix TestUtilities after changing relationships

--- TestUtilities/Data/TestDataHelper.cs.cs
+++ TestUtilities/Data/TestDataHelper.cs.cs
@@ -12,8 +12,6 @@ namespace TestUtilities.Data
                 new Book { 
                     Id = 1, 
                     Title = "1984", 
-                    AuthorId = 1, 
-                    GenreId = 1, 
                     PublisherId = 1, 
                     Price = 15.99M, 
                     Description = "Nineteen Eighty-Four (also published as 1984) is a dystopian novel and cautionary tale by English writer George Orwell. It was published on 8 June 1949" 
@@ -21,8 +19,6 @@ namespace TestUtilities.Data
                 new Book { 
                     Id = 2, 
                     Title = "Harry Potter and the Philosopher's Stone", 
-                    AuthorId = 2, 
-                    GenreId = 2, 
                     PublisherId = 2, 
                     Price = 20.99M, 
                     Description = "Harry Potter and the Philosopher's Stone is a fantasy novel written by British author J. K. Rowling." },
