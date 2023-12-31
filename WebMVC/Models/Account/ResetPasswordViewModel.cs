7e2f3140abf0765c31d84971f0f8f5f1d506acce
Jakub Zovak <482857@mail.muni.cz>
2023-12-31 10:59:15 +0100

    Reset Password done

---
+++ WebMVC/Models/Account/ResetPasswordViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebMVC.Models
+{
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public string Token { get; set; }
+    }
+}
